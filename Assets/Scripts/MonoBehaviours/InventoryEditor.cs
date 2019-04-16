c2e5181b5a6252128dbba2dcb23ec88336216d72
Hernán <hernan.gonzalez.calderon@gmail.com>
2019-04-16 17:10:17 +0100

    Inventory minor changes.

--- Assets/Scripts/MonoBehaviours/InventoryEditor.cs
+++ Assets/Scripts/MonoBehaviours/InventoryEditor.cs
@@ -1,49 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
-
-[CustomEditor (typeof(Inventory))]
+[CustomEditor(typeof(Inventory))]
 public class InventoryEditor : Editor
 {
+	private bool[] showItemSlots = new bool[Inventory.numItemSlots];
 	private SerializedProperty itemImagesProperty;
 	private SerializedProperty itemsProperty;
-	private bool[] showItemsSlot = new bool[Inventory.numItemSlots];
-
 	private const string inventoryPropItemImagesName = "itemImages";
-	private const string inventoryPropItemsName = "item";
-
+	private const string inventoryPropItemsName = "items";
 	private void OnEnable()
 	{
 		itemImagesProperty = serializedObject.FindProperty(inventoryPropItemImagesName);
 		itemsProperty = serializedObject.FindProperty(inventoryPropItemsName);
 	}
-
 	public override void OnInspectorGUI()
 	{
 		serializedObject.Update();
-
 		for (int i = 0; i < Inventory.numItemSlots; i++)
 		{
 			ItemSlotGUI(i);
 		}
-
 		serializedObject.ApplyModifiedProperties();
 	}
-
 	private void ItemSlotGUI(int index)
 	{
 		EditorGUILayout.BeginVertical(GUI.skin.box);
 		EditorGUI.indentLevel++;
 
-		showItemsSlot[index] = EditorGUILayout.Foldout(showItemsSlot[index], "Item slot " + index);
-
-		if (showItemsSlot[index])
+		showItemSlots[index] = EditorGUILayout.Foldout(showItemSlots[index], "Item slot " + index);
+		if (showItemSlots[index])
 		{
 			EditorGUILayout.PropertyField(itemImagesProperty.GetArrayElementAtIndex(index));
-			EditorGUILayout.PropertyField(itemsProperty.GetArrayElementAtIndex(index))
+			EditorGUILayout.PropertyField(itemsProperty.GetArrayElementAtIndex(index));
 		}
-
 		EditorGUI.indentLevel--;
 		EditorGUILayout.EndVertical();
 	}
