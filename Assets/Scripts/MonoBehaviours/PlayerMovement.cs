6ee232ef24fbdcc0dbc46eac2a3c500d9603ab43
Hernán <hernan.gonzalez.calderon@gmail.com>
2019-06-05 00:23:47 +0200

    Adapting adventure tutorial: player movement.

--- Assets/Scripts/MonoBehaviours/PlayerMovement.cs
+++ Assets/Scripts/MonoBehaviours/PlayerMovement.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -10,21 +12,34 @@ public class PlayerMovement : MonoBehaviour
     public float baseSpeed = 2f;
     public float acceleration = 20f;
     public float stoppingDistance = 0.15f;
+	public float slowingSpeed = 0.175f;
 
     private WaitForSeconds inputHoldWait;
     private float speed;
-    private Vector2 destinationPosition;
+	private float maxDistance;
+	private WalkablePath walkablePath;
+	private Collider2D walkablePathCollider;
+	private Vector2 destinationPosition;
     private Vector2 currentPosition;
+	private bool handleInput = true;
+	private Interactable currentInteractable;
+	private GameObject characterSprite;
 
-    private const float stopDistanceProportion = 0.1f;
+	private const float stopDistanceProportion = 0.1f;
+
+	private readonly int hashLocomotionTag = Animator.StringToHash("Locomotion");
 
 	// Start is called before the first frame update
 	private void Start()
     {
         this.inputHoldWait = new WaitForSeconds(inputHoldDelay);
         this.rigidBody = this.GetComponent<Rigidbody2D>();
+		this.walkablePath = FindObjectOfType<WalkablePath>();
+		this.walkablePathCollider = this.walkablePath.GetComponent<Collider2D>();
+		this.characterSprite = this.transform.Find("CharacterSprite").gameObject;
 
-        destinationPosition = this.transform.position;
+		this.transform.position = this.ClampPosition(this.transform.position);
+		destinationPosition = this.transform.position;
     }
 
 
@@ -34,33 +49,46 @@ public class PlayerMovement : MonoBehaviour
     }
     // Update is called once per frame
     private void Update()
-    {
-        float remainingDistance = Vector2.Distance(this.destinationPosition, this.currentPosition);
-        this.currentPosition = this.ClampPosition(this.rigidBody.transform.position);
+	{
+		this.currentPosition = this.ClampPosition(this.transform.position);
+		float remainingDistance = Vector2.Distance(this.destinationPosition, this.currentPosition);
+
+		if (remainingDistance <= this.stoppingDistance)
+		{
+			Stopping();
+		}
+
+		else if (remainingDistance > this.stoppingDistance)
+		{
+			Slowing(remainingDistance);
+		}
 
 
-        if (remainingDistance <= this.stoppingDistance * stopDistanceProportion)
-        {
-            Stopping(out speed);
-        }
+	}
 
-
+	private void Stopping()
+	{
+		this.animator.SetBool("playerIsMoving", false);
+		this.transform.position = this.destinationPosition;
     }
 
-    private void Stopping(out float speed)
-    {
+    private void Slowing(float distanceToDestination)
+	{
+		this.animator.SetBool("playerIsMoving", true);
+		transform.position = this.walkablePathCollider.ClosestPoint(Vector2.MoveTowards(transform.position, this.destinationPosition, this.slowingSpeed * Time.deltaTime));
 
-    }
+		if ((this.destinationPosition.x - this.currentPosition.x) >= 0)
+		{
+			this.characterSprite.transform.localScale = new Vector3(Math.Abs(this.characterSprite.transform.localScale.x), this.characterSprite.transform.localScale.y, this.characterSprite.transform.localScale.z);
 
-    private void Slowing(out float speed, float distanceToDestination)
-    {
+		} else
+		{
+			this.characterSprite.transform.localScale = new Vector3(-Math.Abs(this.characterSprite.transform.localScale.x), this.characterSprite.transform.localScale.y, this.characterSprite.transform.localScale.z);
 
-    }
+		}
+	}
+	
 
-    private void Moving()
-    {
-
-    }
     private Vector2 ClampPosition(Vector2 position)
     {
         Vector2 clamped_position = new Vector2((int)position.x, (int)position.y);
@@ -68,4 +96,32 @@ public class PlayerMovement : MonoBehaviour
 
         return position;
     }
+
+	public void OnGroundClick(BaseEventData data)
+	{
+		if (!this.handleInput)
+		{
+			return;
+		}
+
+		this.currentInteractable = null;
+
+		PointerEventData pData = (PointerEventData)data;
+		this.maxDistance = Time.deltaTime * this.slowingSpeed;
+		this.destinationPosition = this.walkablePathCollider.ClosestPoint(pData.pointerCurrentRaycast.worldPosition);
+	}
+
+	private IEnumerator WaitForInteraction()
+	{
+		this.handleInput = false;
+
+		yield return inputHoldWait;
+
+		while (animator.GetCurrentAnimatorStateInfo(0).tagHash != this.hashLocomotionTag)
+		{
+			yield return null;
+		}
+
+		this.handleInput = true;
+	}
 }
